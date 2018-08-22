5ab0ea11e553c78a7ab403c3af876cfd061af712
bbertka-pivotal <bbertka@pivotal.io>
2018-08-22 15:22:24 -0700

    Fixed typo in JSON

--- install-pcf/aws/tasks/config-director/task.sh
+++ install-pcf/aws/tasks/config-director/task.sh
@@ -30,7 +30,7 @@ read -r -d '' director_configuration <<EOF
   "ntp_servers_string": "0.amazon.pool.ntp.org,1.amazon.pool.ntp.org,2.amazon.pool.ntp.org,3.amazon.pool.ntp.org",
   "resurrector_enabled": true,
   "max_threads": 30,
-  "database_type": "internal"
+  "database_type": "internal",
   "blobstore_type": "s3",
   "s3_blobstore_options": {
     "endpoint": "$S3_ENDPOINT",
