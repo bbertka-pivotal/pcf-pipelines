ca664360c947ceb76463f4396abf507678445d23
bbertka-pivotal <bbertka@pivotal.io>
2018-08-22 21:16:05 -0700

    removed system DB specific value to use internal

--- tasks/configure-ert/task.sh
+++ tasks/configure-ert/task.sh
@@ -231,7 +231,6 @@ cf_properties=$(
       ".properties.container_networking_interface_plugin.silk.network_cidr": { "value": $container_networking_nw_cidr },
       ".properties.route_services.enable.ignore_ssl_cert_verification": { "value": true },
       ".properties.security_acknowledgement": { "value": $security_acknowledgement },
-      ".properties.system_database": { "value": "internal" },
       ".properties.push_apps_manager_company_name": { "value": "pcf-\($iaas)" },
       ".cloud_controller.system_domain": { "value": $system_domain },
       ".cloud_controller.apps_domain": { "value": $apps_domain },
